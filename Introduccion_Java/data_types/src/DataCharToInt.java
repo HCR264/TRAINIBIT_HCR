0b2350b5077949881c169d7e114534271328cbb3
Harold Cruz-Ramirez <haroldcruzramirez@gmail.com>
2025-08-26 09:23:13 -0600

    Conversión de tipo de datos

---
+++ Introduccion_Java/data_types/src/DataCharToInt.java
@@ -0,0 +1,7 @@
+public class DataCharToInt {
+    public static void main(String[] args) {
+        char character = 'a';
+        System.out.println("Valor almacenado en character: " + character);
+        System.out.println("Valor de character convertido a int: " + (int)character);
+    }
+}
